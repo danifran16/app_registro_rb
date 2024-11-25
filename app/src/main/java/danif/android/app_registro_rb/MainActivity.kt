ca18cd69f7213ed49ddffa77e6cdccb0e8eedd79
danifran16 <daniela.frangb@gmail.com>
2024-11-25 01:14:55 -0300

    Se agrega funcionalidad de agregar registros

--- app/src/main/java/danif/android/app_registro_rb/MainActivity.kt
+++ app/src/main/java/danif/android/app_registro_rb/MainActivity.kt
@@ -6,33 +6,44 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import danif.android.app_registro_rb.ui.theme.App_registro_rbTheme
+import danif.android.app_registro_rb.modelo.RegistroClass
 
 class MainActivity : ComponentActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,8 +58,10 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = false, showSystemUi = true)
 @Composable
 fun AppRegistroServicios(
-    navController: NavHostController = rememberNavController()
+    navController: NavHostController = rememberNavController(),
 ) {
+    val listaRegistros = remember { mutableStateListOf<RegistroClass>() }
+
     NavHost(
         navController = navController,
         startDestination = "inicio"
@@ -57,15 +70,16 @@ fun AppRegistroServicios(
             PageInicioUI(
                 onButtonSettingsClicked = {
                     navController.navigate("agregarRegistros")
-                }
-//                navController
+                },
+                listaRegistros
             )
         }
         composable("agregarRegistros") {
             AgregarRegistrosPageUI(
                 onBackButtonClicked = {
                     navController.popBackStack()
-                }
+                },
+                listaRegistros
             )
         }
     }
@@ -99,8 +113,9 @@ fun BannerNav(
         actions = {
             if( showSettingsButton ) {
                 IconButton(onClick = {
-                    onButtonSettingsClicked()
-                }) {
+                        onButtonSettingsClicked()
+                    }
+                ) {
                     Icon(
                         imageVector = Icons.Filled.Settings,
                         contentDescription = "Configuración"
@@ -108,10 +123,10 @@ fun BannerNav(
                 }
             }
         },
-//        colors = TopAppBarDefaults.topAppBarColors(
-//            containerColor = MaterialTheme.colorScheme.primaryContainer,
-//            titleContentColor = MaterialTheme.colorScheme.primary
-//        )
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary
+        )
     )
 }
 
@@ -121,55 +136,70 @@ fun BannerNav(
 @Preview(showSystemUi = true)
 @Composable
 fun PageInicioUI(
-    onButtonSettingsClicked:() -> Unit = {}
-//            navController: NavController)
-)
-{
-
+    onButtonSettingsClicked:() -> Unit = {},
+    registros: MutableList<RegistroClass>
+) {
     Scaffold(
         topBar = {
             BannerNav(
                 title = "Listado Tareas",
                 onButtonSettingsClicked = onButtonSettingsClicked
             )
+        },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+                Text(
+                    text = "Registros"
+                )
+
+                LazyColumn(modifier = Modifier.fillMaxSize().padding(vertical = 16.dp))
+                {
+                    items(registros.size) { index: Int ->
+                        RegistroItem(
+                            registro = registros[index],
+                            onDelete = { registros.removeAt(index) }
+                        )
+                    }
+                }
+            }
         }
     )
-    {
-            paddingValues ->
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ){
-
-            Text(
-                text="Registros"
-            )
-
-//            Button(
-//                onClick = { navController.navigate("agregarRegistros")
-//                }
-//            ) {
-//                Text("Agregar Nuevo Registro")
-//            }
-        }
-    }
-
-
-
-
-
 }
+
+
+@Composable
+fun RegistroItem(
+    registro: RegistroClass,
+    onDelete: () -> Unit
+) {
+    Column {
+        Text(text = "Nombre Registro:  ${registro.registro}")
+        Text(text = "Fecha:  ${registro.fecha}")
+        Text(text = "Tipo Registro:  ${registro.tipoRegistro}")
+    }
+    IconButton(onClick = onDelete) {
+        Icon(
+            imageVector = Icons.Filled.Delete,
+            contentDescription = "EliminarRegistro",
+            tint = Color.Red,
+        )
+    }
+}
+
+
 
 //Pagina de registros
 //@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showSystemUi = true)
 @Composable
 fun AgregarRegistrosPageUI(
-    onBackButtonClicked:() -> Unit = {}
+    onBackButtonClicked:() -> Unit = {},
+    registros: MutableList<RegistroClass>
 ) {
-
     var registroMedidorValue by remember { mutableStateOf("") }
     var registroFechaValue by remember { mutableStateOf("") }
     var selectedOption by remember { mutableStateOf("Agua") }
@@ -182,74 +212,88 @@ fun AgregarRegistrosPageUI(
                 showBackButton = true,
                 onBackButtonClicked = onBackButtonClicked
             )
+        },
+        content = { paddingValues ->
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 30.dp, vertical = paddingValues.calculateTopPadding())
+            ){
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TextField(
+                    value = registroMedidorValue,
+                    placeholder = { Text(text = "ingrese registro") },
+                    onValueChange = { registroMedidorValue = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TextField(
+                    value = registroFechaValue,
+                    placeholder = { Text(text = "ingrese registro") },
+                    onValueChange = { registroFechaValue = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = "Tipo de medidior:",
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                //Opcion Agua
+                Row {
+                    RadioButton(
+                        selected = selectedOption == "Agua",
+                        onClick = { selectedOption = "Agua" }
+                    )
+                    Text(
+                        text = "Agua"
+                    )
+                }
+                //Opcion Luz
+                Row {
+                    RadioButton(
+                        selected = selectedOption == "Luz",
+                        onClick = { selectedOption = "Luz" }
+                    )
+                    Text(
+                        text = "Luz"
+                    )
+                }
+                //Opcion Gas
+                Row {
+                    RadioButton(
+                        selected = selectedOption == "Gas",
+                        onClick = { selectedOption = "Gas" }
+                    )
+                    Text(
+                        text = "Gas"
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = {
+                        val registro = RegistroClass(
+                            registro = registroMedidorValue,
+                            fecha = registroFechaValue,
+                            tipoRegistro = selectedOption
+                        )
+
+                        registros.add(registro)
+                    }
+                ) {
+                Text("Agregar Registro")
+                }
+            }
         }
     )
-
-    {
-        Column(
-            modifier = Modifier
-                .padding(
-                    horizontal = 10.dp,
-                    vertical = it.calculateTopPadding()
-                )
-        )
-
-        {
-            TextField(
-                value = registroMedidorValue,
-                placeholder = { Text(text = "ingrese registro") },
-                onValueChange = { registroMedidorValue = it }
-            )
-
-            TextField(
-                value = registroFechaValue,
-                placeholder = { Text(text = "ingrese registro") },
-                onValueChange = { registroFechaValue = it }
-            )
-
-            Text(
-                text = "Tipo de medidior:",
-            )
-
-            //Opcion Agua
-            Row {
-                RadioButton(
-                    selected = selectedOption == "Agua",
-                    onClick = { selectedOption = "Agua" }
-                )
-                Text(
-                    text = "Agua"
-                )
-            }
-
-            //Opcion Luz
-            Row {
-                RadioButton(
-                    selected = selectedOption == "Luz",
-                    onClick = { selectedOption = "Luz" }
-                )
-                Text(
-                    text = "Luz"
-                )
-            }
-
-            //Opcion Gas
-            Row {
-                RadioButton(
-                    selected = selectedOption == "Gas",
-                    onClick = { selectedOption = "Gas" }
-                )
-                Text(
-                    text = "Gas"
-                )
-            }
-
-            Button(onClick = {}) {
-                Text("Agregar Registro")
-            }
-        }
-
-
-    }
-
 }
---
+++ app/src/main/java/danif/android/app_registro_rb/modelo/Registro.kt
@@ -0,0 +1,12 @@
+package danif.android.app_registro_rb.modelo
+import java.io.Serializable
+
+data class RegistroClass(
+    val registro:String,
+    val fecha:String,
+    val tipoRegistro:String
+
+) : Serializable
+
+
+
